ffd9f8088fb552a1a0b675f120e6aace35c4ac59
carloslopez98 <carlos980718@gmail.com>
2025-10-17 19:10:00 -0500

    feat: Add the  query in the Trivia repository

--- src/main/java/com/group2/whos_that_monster/repository/TriviaRepository.java
+++ src/main/java/com/group2/whos_that_monster/repository/TriviaRepository.java
@@ -2,6 +2,9 @@ package com.group2.whos_that_monster.repository;
 
 import com.group2.whos_that_monster.model.Trivia;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface TriviaRepository extends JpaRepository<Trivia, Long> {
+    @Query(value = "SELECT * FROM trivia ORDER BY RANDOM LIMIT 1", nativeQuery = true)
+    Trivia findRandom();
 }
