d5f742e4a8e0d0b4314c7504626b52d7134cf3c3
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2025-10-18 14:05:28 -0500

    feat: add Monster entity

---
+++ src/main/java/com/group2/whos_that_monster/entity/Monster.java
@@ -0,0 +1,30 @@
+package com.group2.whos_that_monster.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "monster")
+public class Monster {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "image_silhouette")
+    private String imageSilhouette;
+
+    @Column(name = "image_real")
+    private String imageReal;
+}
