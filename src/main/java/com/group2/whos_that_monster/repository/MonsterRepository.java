aad67633c66e39efe45d020e90d9cfaed6d139a4
carloslopez98 <carlos980718@gmail.com>
2025-10-20 19:51:59 -0500

    feat: Trivia and Option repositories

--- src/main/java/com/group2/whos_that_monster/repository/MonsterRepository.java
+++
@@ -1,7 +0,0 @@
-package com.group2.whos_that_monster.repository;
-
-import com.group2.whos_that_monster.model.Monster;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MonsterRepository extends JpaRepository<Monster, Long> {
-}
--- src/main/java/com/group2/whos_that_monster/repository/TriviaRepository.java
+++ src/main/java/com/group2/whos_that_monster/repository/TriviaRepository.java
@@ -1,6 +1,6 @@
 package com.group2.whos_that_monster.repository;
 
-import com.group2.whos_that_monster.model.Trivia;
+import com.group2.whos_that_monster.entity.Trivia;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
