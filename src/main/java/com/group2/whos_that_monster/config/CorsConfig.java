f215e21cd700fedccd2e36e21342b57bd8f65a5c
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2025-10-29 15:55:42 -0500

    feat: add CorsConfig G,P,P,D

---
+++ src/main/java/com/group2/whos_that_monster/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.group2.whos_that_monster.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")  // o tu URL de ngrok
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+            }
+        };
+    }
+}
